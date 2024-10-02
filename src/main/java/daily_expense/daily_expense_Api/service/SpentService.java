bee632fad2c32260a86bfdc567f77021915fd7fe
LucasLouzano <lucas_louzano@hotmail.com>
2024-10-02 17:25:54 -0300

    Implementação na interface SpentService

--- src/main/java/daily_expense/daily_expense_Api/service/SpentService.java
+++ src/main/java/daily_expense/daily_expense_Api/service/SpentService.java
@@ -2,6 +2,12 @@ package daily_expense.daily_expense_Api.service;
 
 import daily_expense.daily_expense_Api.model.Spent;
 
+import java.util.List;
+
 public interface SpentService {
+    Spent findByid(Long id);
+    List<Spent> findAll();
     Spent save(Spent spent);
+    Spent update(Long id, Spent spent);
+    boolean delete(Long id);
 }
