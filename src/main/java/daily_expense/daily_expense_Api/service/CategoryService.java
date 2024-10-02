f420fbbe07e3b6f2bf2f3adfaee0ec20f9d6bf3c
LucasLouzano <lucas_louzano@hotmail.com>
2024-10-02 17:27:28 -0300

    Implementação da interface CategoryService

--- src/main/java/daily_expense/daily_expense_Api/service/CategoryService.java
+++ src/main/java/daily_expense/daily_expense_Api/service/CategoryService.java
@@ -1,2 +1,10 @@
-package daily_expense.daily_expense_Api.service;public interface CategoryService {
+package daily_expense.daily_expense_Api.service;
+
+import daily_expense.daily_expense_Api.model.Category;
+
+import java.util.List;
+public interface CategoryService {
+    Category findById(Long id);
+    List<Category> findAll();
+    Category update(Long id, Category category);
 }
