073572ab1c95caa2268315a2ab75a5a48dea0bd1
LucasLouzano <lucas_louzano@hotmail.com>
2024-10-02 17:28:46 -0300

    Implementaação da interface SpentRepository

--- src/main/java/daily_expense/daily_expense_Api/repository/SpentRepository.java
+++ src/main/java/daily_expense/daily_expense_Api/repository/SpentRepository.java
@@ -4,7 +4,9 @@ import daily_expense.daily_expense_Api.model.Spent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpentRepository extends JpaRepository<Spent, Long> {
-
+    List<Spent> findByCategoryId(Long categoryId);
 }
